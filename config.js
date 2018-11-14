3bb745f94531de10bf504d07782526e8a43a6db8
Jung-Been Lee <jungbini@gmail.com>
2018-11-14 13:51:14 +0900

    modify db server address

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     server_port: 3000,
-    db_url: 'postgres://emoodchart:esel10582@jungbini.com:5432/mobileserver',    
+    db_url: 'postgres://emoodchart:esel10582@bipolar-database.ckq0ra1dncd4.ap-northeast-2.rds.amazonaws.com:5432/emoodchart',    
     fcm_api_key: 'AAAAoXvvNNw:APA91bHcTpYTqup6kk_nFwi0EQ2X1MVYeBdTmT_ApNtOHOlWWW_1m1hxpatDhu7QQ8M0J9FkOe8DIQf6H0KpTYhWO8OJ5_9sm9JH69FHRM1uKorAW_L0wUDSarWTP5NUYZ1cCO6uE6tlpfTWi5he4caP1y4ALrBtQA',
     project_id: '693569008860',
     notification_key: 'APA91bFYPrWpggOKUAhtRdPs25Ed6xvBl9UXbJlUd3UWaxhycRMB-jXO75jOTFQeLUHrEwsLWj_88KxlNrw16TPdkWWceed4WTryanUc0xsBba7G0ZxoRwk',
